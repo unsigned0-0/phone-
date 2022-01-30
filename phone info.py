2967dcbb1bc3c56979ce48707f691080207a5b75
unsigned0-0 <88989398+unsigned0-0@users.noreply.github.com>
2022-01-30 13:30:08 -0800

    Add files via upload

---
+++ phone info.py
@@ -0,0 +1,43 @@
+LIST=int(input('Type the number you are searching for..  '))
+print(f'{LIST}\n')
+
+LISTS =  {
+    1111: 'Amal',
+    2222: 'maha',
+    3333: 'mohammed',
+}
+print(f'there you go {LISTS[LIST]}\n ')
+#pop up ass hole 
+while True:
+    if str(input('do you wnat to search for another number ')) == 'no':
+        print('P')  
+    else:
+        break
+        
+                   
+
+LIST = int(input('Type the number you are searching for.. '))
+print(f'{LIST}')
+
+LISTS =  {
+    1111: 'Amal',
+    2222: 'maha',
+    3333: 'mohammed',
+}
+print(f'there you go {LISTS[LIST]} ')
+
+#####aaaaaaa##############aaaaaaaaaaaaaa#################################
+POP = int(input('dniodfjpf'))
+print(f'{ POP}')
+pop = {
+    1111:'22222',
+    1222:'44444'
+    
+    
+}
+print(f'{POP[pop]}')
+
+input('do you wnat to search for another number ')
+if input == 'no':
+    exit
+    
